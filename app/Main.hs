e11a9f7d61a8d65ba7daf70a02b9c467d7dfc887
Yanhao Li <hi@yanhaoli.com>
2021-12-18 15:26:33 -0500

    feat: initialize maze

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,14 @@ module Main where
 import System.Exit(die);
 import System.Environment (getArgs, getProgName)
 
-import Maze (Maze, mazeGenerator, printMaze)
+import Maze
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [width, height] -> do
-      printMaze $ mazeGenerator (read width) (read height)
+      print $ mazeGenerator (read width) (read height)
     _ -> do
       progName <- getProgName 
       die $ "Usage: " ++ progName ++ " <width> <height>"
--- src/Maze.hs
+++ src/Maze.hs
@@ -1,13 +1,40 @@
-module Maze
-( Maze
-, mazeGenerator
-, printMaze
-) where
+module Maze where
 
-type Maze = [[Bool]]
+data Wall = Wall {
+  r :: Int,
+  c :: Int
+} deriving (Show, Eq)
+
+data Maze = Maze {
+  m :: Int, -- number of matrix rows
+  n :: Int, -- number of matrix cols
+  walls :: [Wall]
+}
+
+instance Show Maze where
+  show (Maze m n walls) =
+    unlines maze
+    where
+      maze = [rowToStrng r| r <- [0..m-1]]
+      rowToStrng r = [if isWall r c then '#' else ' '| c <- [0..n-1]]
+      isWall r c = Wall r c `elem` walls
 
 mazeGenerator :: Int -> Int -> Maze
-mazeGenerator width height = replicate height $ replicate width False
+mazeGenerator width height =
+  let maze = initializeMaze width height
+  in
+    maze
 
-printMaze :: Maze -> IO ()
-printMaze maze = mapM_ putStrLn $ map (map (\x -> if x then '#' else '.')) maze
+initializeMaze :: Int -> Int -> Maze
+initializeMaze width height =
+  Maze {
+    m = m,
+    n = n,
+    walls = walls
+  }
+  where
+    walls = [ Wall {r = r, c = c} | r <- [0..m - 1], 
+                                    c <- [0..n - 1],
+                                    r == 0 || r == m - 1 || c == 0 || c == n - 1 ]
+    m = width * 2 + 1 -- number of matrix rows
+    n = height * 2 + 1 -- number of matrix cols
