b3529792541622d5de7af1eb342941006d4bb1dc
Yanhao Li <hi@yanhaoli.com>
2021-12-22 22:20:01 -0500

    feat: added third parameter as seed

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,21 @@ module Main where
 import System.Exit(die);
 import System.Environment (getArgs, getProgName)
 
-import System.Random (getStdGen)
 import System.CPUTime
 import Generator
 import Control.DeepSeq
+import System.Random (mkStdGen)
 
-main :: IO Integer
+main :: IO ()
 main = do
   args <- getArgs
-  seed <- getStdGen
   case args of
-    [width, height] -> do
-      start <- getCPUTime
-      let r = mazeGenerator (read width) (read height) seed
-      end <- r `deepseq` getCPUTime
-      return (end - start)
-
+    [width, height, seed] -> do
+      -- start <- getCPUTime
+      -- let r = mazeGenerator (read width) (read height) (mkStdGen (read seed))
+      -- end <- r `deepseq` getCPUTime
+      -- return (end - start)
+      print $ mazeGenerator (read width) (read height) (mkStdGen (read seed))
     _ -> do
       progName <- getProgName 
       die $ "Usage: " ++ progName ++ " <width> <height>"
--- src/Generator.hs
+++ src/Generator.hs
@@ -14,8 +14,7 @@ mazeGenerator width height seed =
   M.Maze {
     M.m = m,
     M.n = n,
-    M.walls = initializeFrame m n `union` fst (generateWalls 0 0 (m - 1) (n - 1) seed)
-    -- M.walls = initializeFrame m n
+    M.walls = initializeFrame m n `union` generateWalls 0 0 (m - 1) (n - 1) seed
   }
   where
     m = width * 2 + 1 -- number of matrix rows
@@ -34,38 +33,43 @@ initializeFrame m n = fromList frame
 -- Given an frame, generate walls inside the frame
 -- top left wall cell: (tr, tc)
 -- bottom right wall cell: (br, bc)
-generateWalls :: RandomGen g => Int -> Int -> Int -> Int -> g -> (Set W.Wall, g)
-generateWalls tr tc br bc seed
-  -- | trace ("generateHWalls for the space: " ++ show (tx, ty, bx, by)) False = undefined
-  -- | trace ("holes: " ++ show holes) False = undefined
-  | bc - tc < 4 || br - tr < 4 = (empty, seed)
-  | otherwise = (walls
+generateWalls :: RandomGen g => Int -> Int -> Int -> Int -> g -> Set W.Wall
+generateWalls tr tc br bc g
+  | bc - tc < 4 || br - tr < 4 = empty
+  | otherwise = walls
     `union` topLeft 
     `union` topRight 
     `union` bottomLeft 
-    `union` bottomRight, newSeed5)
+    `union` bottomRight
   where
-    (randomRow, newRowSeed) = pickRandom [(tr + 2), (tr + 4)..(br - 2)] seed
-    (randomCol, newColSeed) = pickRandom [(tc + 2), (tc + 4)..(bc - 2)] newRowSeed
+    (g1, g2) = split g
+    (g3, g4) = split g1
+    (g5, g6) = split g2
+    (g7, g8) = split g3
+    (randomRow, _) = pickRandom [(tr + 2), (tr + 4)..(br - 2)] g1
+    (randomCol, _) = pickRandom [(tc + 2), (tc + 4)..(bc - 2)] g2
     walls = verticalWalls `union` horizontalWalls `difference` holes
     verticalWalls = fromList [W.Wall {W.r = r, W.c = randomCol} | r <- [(tr + 1)..(br - 1)]]
     horizontalWalls = fromList [W.Wall {W.r = randomRow, W.c = c} | c <- [(tc + 1)..(bc - 1)]]
-    (topLeft, newSeed1) = generateWalls tr tc randomRow randomCol seed
-    (topRight, newSeed2) = generateWalls tr randomCol randomRow bc seed
-    (bottomLeft, newSeed3) = generateWalls randomRow tc br randomCol seed
-    (bottomRight, newSeed4) = generateWalls randomRow randomCol br bc seed
-    (holes, newSeed5) = getHoles tr tc br bc randomRow randomCol seed
+    topLeft = generateWalls tr tc randomRow randomCol g3
+    topRight = generateWalls tr randomCol randomRow bc g4
+    bottomLeft = generateWalls randomRow tc br randomCol g5
+    bottomRight = generateWalls randomRow randomCol br bc g6
+    (holes, _) = getHoles tr tc br bc randomRow randomCol g7
 
 getHoles :: RandomGen g => Int -> Int -> Int -> Int -> Int -> Int -> g -> (Set W.Wall, g)
-getHoles tr tc br bc rr rc seed = 
-  (fromList $ map (choices !!) $ take 3 $ shuffle seed [0..3], bottomSeed)
+getHoles tr tc br bc rr rc g = 
+  (fromList $ map (choices !!) $ take 3 $ shuffle g [0..3], g5)
   where
-    randomPick l seed = l !! fst (randomR (0, length l - 1) seed)
+    (g1, g2) = split g
+    (g3, g4) = split g1
+    (g5, g6) = split g2
+    randomPick l seed = l !! fst (randomR (0, length l - 1) g6)
     choices = [top, left, right, bottom] 
-    (top, topSeed) = pickRandom [W.Wall {W.r = r, W.c = rc}| r <- [(tr + 1), (tr + 3)..(rr - 1)]] seed
-    (left, leftSeed) = pickRandom [W.Wall {W.r = rr, W.c = c}| c <- [(tc + 1), (tc + 3)..(rc - 1)]] topSeed
-    (right, rightSeed) = pickRandom [W.Wall {W.r = rr, W.c = c}| c <- [(rc + 1), (rc + 3)..(bc - 1)]] leftSeed
-    (bottom, bottomSeed) = pickRandom [W.Wall {W.r = r, W.c = rc}| r <- [(rr + 1), (rr + 3)..(br - 1)]] rightSeed
+    (top, topSeed) = pickRandom [W.Wall {W.r = r, W.c = rc}| r <- [(tr + 1), (tr + 3)..(rr - 1)]] g1
+    (left, leftSeed) = pickRandom [W.Wall {W.r = rr, W.c = c}| c <- [(tc + 1), (tc + 3)..(rc - 1)]] g2
+    (right, rightSeed) = pickRandom [W.Wall {W.r = rr, W.c = c}| c <- [(rc + 1), (rc + 3)..(bc - 1)]] g3
+    (bottom, bottomSeed) = pickRandom [W.Wall {W.r = r, W.c = rc}| r <- [(rr + 1), (rr + 3)..(br - 1)]] g4
 
 pickRandom :: RandomGen g => [a] -> g -> (a, g)
 pickRandom l seed = Data.Bifunctor.first (l !!) (randomR (0, length l - 1) seed)
